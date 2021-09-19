698517fb3677783dd4acc1eed81beec3d034ad34
ScrawnyViking <info@tbc.exchange>
2021-09-19 06:20:51 -0500

    https://TBC.Exchange TBC Token Presale through NFTs!

--- migrations/2_smart_contract_migration.js
+++ migrations/2_smart_contract_migration.js
@@ -1,5 +1,5 @@
 const SmartContract = artifacts.require("TheBeeCollaborative"); // Change this to what your Contract Name is in Solidity
 
 module.exports = function (deployer) {
-  deployer.deploy(SmartContract, "Name", "Symbol", "https://");
+  deployer.deploy(SmartContract, "Name", "Symbol", "https://"); //
 };
